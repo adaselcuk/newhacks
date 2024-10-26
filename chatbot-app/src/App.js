2f0b046bf4be59216c4b92bb857c9b5e2ee24fcd
Ada <adaselcuk16@gmail.com>
2024-10-26 17:43:43 -0400

    backend for bot done!!!

--- chatbot-app/src/App.js
+++ chatbot-app/src/App.js
@@ -4,25 +4,18 @@ import axios from 'axios';
 import Home from './Home';
 import Map from './Map'; 
 import Todo from './Todo';
+import Bot from './Bot';
 import './App.css';
 
 function App() {
-  const [response, setResponse] = useState("Hi there! Natural disasters are bad");
-  const [value, setValue] = useState("");
 
-  const onChange = (e) => {setValue(e.target.value);};
-  const handleSubmit = async () => {
-    const response = await axios.post("http://localhost:3000/chatbot", {
-      question: value,
-    });
-    setResponse(response.data);
-  };
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/map" element={<Map />} />
         <Route path="/todo" element={<Todo />} />
+        <Route path="/bot" element={<Bot/>} />
 
       </Routes>
     </Router>
