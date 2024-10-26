38f3997110f868306f5d8da4d6ffcc4d5e2283a6
Ada <adaselcuk16@gmail.com>
2024-10-26 13:19:46 -0400

    applied chatbot functionality - VERY BASIC AND HAS ERRORS

--- chatbot-app/src/App.js
+++ chatbot-app/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
+  const [response, setResponse] = useState("Hi there! Natural disasters are bad");
+  const [value, setValue] = useState("");
+
+  const onChange = (e) => {setValue(e.target.value);};
+  const handleSubmit = async () => {
+    const response = await axios.post("http://localhost:3000/chatbot", {
+      question: value,
+    });
+    setResponse(response.data);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <div>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+        ></input>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>Click me for answers!</button>
+      </div>
+      <div>
+        <p>Chatbot: {response}</p>
+      </div>
     </div>
   );
 }
--- server/server.js
+++
@@ -1,5 +0,0 @@
-import express from "express";
-import cors from "cors";
-import { OpenAI } from "openai";
-import dotenv from "dotenv";
-
