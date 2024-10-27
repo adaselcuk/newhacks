b731eef1ac168a58da7ec5662983c064b6386d5e
Ada <adaselcuk16@gmail.com>
2024-10-27 07:54:01 -0400

    changes to home page and map

--- chatbot-app/src/Home.js
+++ chatbot-app/src/Home.js
@@ -10,45 +10,27 @@ function Home() {
     <div className="App">
       <div className="navbar">
         <ul>
-          <li>Logo</li>
-          <li id="right-corner" className="volunteers-link">
-            <Link to="/todo">Volunteers Click Here</Link></li>
+          <li className="logo">Rescue Hub</li>
+          <li id="right-corner" className="volunteers-link colour">
+            <Link to="/todo">Volunteers Click Here!</Link></li>
+            <li className="colour">
+            <Link to="/community">Community Forum</Link></li>
+            <li className="colour">
+            <Link to="/gembot">Our AI Bot</Link></li>
+            <li className="colour">
+            <Link to="/map">Safety Zones</Link></li>
         </ul>
+        
       </div>
-      <div id="heading">Disaster Averter</div>
+      <div id="heading"><h1>Rescue Hub</h1></div>
       <div id="subheading">Get information about how to prepare for natural disasters and safe evacuation zones before they hit</div>
       <div className="content-container">
       <div className="chatbot-container">
+
         <GemBot />
       </div>
-      <div className="map-container">
-        <MapContainer center={[43.6532, -79.3832]} zoom={13}>
-          <TileLayer 
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-
-          
-        </MapContainer>
-        <Link to="/map">
-            <button id="but1">Nearest Evacuation Sites</button>
-          </Link>
+    
       </div>
-
-      </div>
-      <div className="cards-container">
-        <Link to="/community">
-        <Card title="Ask for help from or give back to the community here" />
-        </Link>
-      </div>
-    </div>
-  );
-}
-
-function Card({ title }) {
-  return (
-    <div className="card">
-      <h3>{title}</h3>
     </div>
   );
 }
